169d8516a089f205b2eaf8363d2be4961158b707
alesk1v9 <alexsanderrafael2022@gmail.com>
2024-03-24 10:17:30 -0400

    added init function/prompt questions

--- index.js
+++ index.js
@@ -4,10 +4,65 @@ const cTable = require('console.table');
 
 const db = mysql.createConnection(
     {
-    host: 'localhost',
-    user: 'root',
-    password: 'mortadela1',
-    database: 'employee_db'
-},
+        host: 'localhost',
+        user: 'root',
+        password: 'mortadela1',
+        database: 'employee_db'
+    },
 
-console.log('connected to employee_db'));
+    console.log('connected to employee_db'));
+
+const questions = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'choice',
+        choices:
+            [
+                'view all departments',
+                'view all roles',
+                'view all employees',
+                'add a department',
+                'add a role',
+                'add an employee',
+                'update an employee role',
+                'exit'
+            ]
+
+    }
+];
+
+
+function init(){
+    inquirer.prompt(questions)
+    .then((response) => {
+        switch(response.choice){
+            case 'view all departments':
+            viewAllDepartments();
+            break;
+            case 'view all roles':
+            viewAllRoles();
+            break;
+            case 'view all employees':
+            viewAllEmployees();
+            break;
+            case 'add a department':
+            addDepartment();
+            break;
+            case 'add a role':
+            addRole();
+            break;
+            case 'add an employee':
+            addEmployee();
+            break;
+            case 'update an employee role':
+            updateEmployee();
+            break;
+            case 'exit':
+            console.log('You ended the app, bye!');
+            process.exit();
+        };
+      });
+  };
+
+  init();
